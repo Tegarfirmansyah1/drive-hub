a884a4e159dd40e698fbd10d49bf53b3598d616d
Tegarfirmansyah1 <tegarf555@gmail.com>
2026-06-30 00:48:53 +0700

    feat: add Google site verification meta tag to RootLayout

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,6 +27,9 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
+      <head>
+        <meta name="google-site-verification" content="HbD75JnRgZDKlZ052B6uvYpcY40NVAIdJuyUQEXVkAc" />
+      </head>
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
   );
